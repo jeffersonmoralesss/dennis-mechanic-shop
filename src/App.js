e9673e758966dcdfb93df62a18b05c79fc7bea3d
Jefferson Morales <jeffersonmorales@jeffersons-air.lan>
2025-08-12 01:46:05 -0400

    fxed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/header';
 
 import Home from './pages/Home';
 import Services from './pages/Services';
-import Reservation from './pages/Reservation';
 import About from './pages/About';
 import Commercial from './pages/Commercial';
 
@@ -22,7 +21,6 @@ function App() {
       <nav style={styles.nav}>
         <Link to="/" style={styles.link}>Home</Link>
         <Link to="/services" style={styles.link}>Services</Link>
-        <Link to="/reservation" style={styles.link}>Reservation</Link>
         <Link to="/about" style={styles.link}>About</Link>
         <Link to="/commercial" style={styles.link}>Commercial</Link>
       </nav>
@@ -31,7 +29,6 @@ function App() {
         {/* Existing routes */}
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/reservation" element={<Reservation />} />
         <Route path="/about" element={<About />} />
         <Route path="/commercial" element={<Commercial />} />
 
