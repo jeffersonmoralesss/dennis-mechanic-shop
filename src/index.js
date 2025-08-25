1fc07a0b56fb7785bed4783eee75907a3d71f5b9
Jefferson Morales <jeffersonmorales1022@gmail.com>
2025-08-25 16:13:37 -0400

    index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
+
+// If you are using i18n (translations)
+import { I18nProvider } from './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <I18nProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nProvider>
+  </React.StrictMode>
+);
