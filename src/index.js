6829a4e5155eda58b30949a36901ab1687acdc26
Jefferson Morales <jeffersonmorales1022@gmail.com>
2025-08-25 16:34:45 -0400

    index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 // Use your i18n provider (must be a named export: I18nProvider)
-import { I18nProvider } from './i18n';
+import I18nProvider from './i18n';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
